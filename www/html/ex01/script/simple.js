994c13c0ea811eda58fe104ab8ed3ecd2599f9b6
細田空冶 <qooooo@hosodakuunoAir.AirPort>
2022-02-08 12:15:14 +0900

    Initial

--- www/html/ex01/script/simple.js
+++ www/html/ex01/script/simple.js
@@ -13,30 +13,3 @@ $(document).ready(function(){
         });
 });
 
-
-window.addEventListener('DOMContentLoaded', function(){
-
-	var input_file = document.querySelector('[name=input_file]');
-
-	input_file.addEventListener('change', function(e){
-
-		if( e.target.files[0].type === 'image/jpeg' || e.target.files[0].type === 'image/png' ) {
-
-
-			// img要素を作成
-			var img_element = document.createElement("img");
-			img_element.src = e.target.files[0].name;
-			img_element.alt = "アップロードに成功しました";
-			img_element.width = 100;
-			img_element.onload = function(){
-				URL.revokeObjectURL(this.src);
-			}
-
-			// ページにimg要素を挿入して画像ファイルを表示
-			var div_element = document.getElementById('file_viewer');
-			div_element.appendChild(img_element);
-
-
-		}
-	});
-});
